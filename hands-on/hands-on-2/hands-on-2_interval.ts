b0631c8f53d13bcd4fba9d4ac34ab49724e0a114
Lorenzo! <18583152+lorenzojkrl@users.noreply.github.com>
2022-03-19 11:07:58 +0200

    RxJS interval solution

--- hands-on/hands-on-2/hands-on-2_interval.ts
+++ hands-on/hands-on-2/hands-on-2_interval.ts
@@ -5,3 +5,9 @@ const myObservable = interval(1000);
 myObservable.subscribe((integer) => {
   console.log(integer);
 });
+
+// Solution
+// the observable complete if the value of integer is >= 10
+myObservable.subscribe((integer) => {
+  integer < 10 ? console.log(integer) : () => {};
+});
